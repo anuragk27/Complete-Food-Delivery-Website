5c75fa0577f31118ec8ba43a34b64848592cb685
Anurag kumar <anuragk.c11@gmail.com>
2024-09-19 14:46:59 +0530

    complete frontend

--- frontend/src/context/storeContext.jsx
+++ frontend/src/context/storeContext.jsx
@@ -20,11 +20,23 @@ const StoreContextProvider = (props) => {
         setcartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                let itemInfo = food_list.find((product) => product._id == item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
+
     const contextValue = {
         food_list,
         cartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
     return (
         <StoreContext.Provider value={contextValue}>
